ab3135b72fbc46f53cd7663af20ebc4fe0f6312e
RohitPawar001 <rppawar491@gmail.com>
2025-02-15 21:58:36 +0530

    image descripter added

--- src/image_descripter/image_descripter.py
+++ src/image_descripter/image_descripter.py
@@ -1,1 +1,20 @@
-"""Main module."""
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.2)
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "you are an helpfull assistance give the description about the user query"),
+        ("user", "Tell me something about {topic}")
+    ]
+)
+output_parser = StrOutputParser()
+chain = prompt_template | llm |output_parser
+
+result = chain.invoke({"topic": "cat"})
+print(result)
